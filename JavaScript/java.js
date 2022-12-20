82d1761d7923ab490451619a27eadbd0eb0be99e
Brunomg00 <120791755+Brunomg00@users.noreply.github.com>
2022-12-20 09:32:29 +0100

    Add files via upload

---
+++ JavaScript/java.js
@@ -0,0 +1,10 @@
+let a;
+let b;
+let c;
+a = 'Hola Mi Gente'
+b = 'Raul eres gay'
+c = 'You Have Been Hacked'
+
+document.getElementById("result1").innerHTML
+document.getElementById("result2").innerHTML
+document.getElementById("result3").innerHTML
